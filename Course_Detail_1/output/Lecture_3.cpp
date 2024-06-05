d85910f6379c78c224410eba68b7bb29ffb1dc43
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-06-05 09:43:49 +0545

    declaring variable

---
+++ Course_Detail_1/output/Lecture_3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10; //declare integer variable
+    cout<<"vaiable of integer : "<<a<<"\n";
+    string s="satya Kumar chaudhary";
+    cout<<"The string of variable :"<<s;
+
+    return 0;
+}
