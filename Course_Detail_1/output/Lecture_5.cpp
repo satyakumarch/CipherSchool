c2e8be1e011280e186c4133aaef571d5567c654e
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-06-06 22:36:31 +0545

    conditional statement

---
+++ Course_Detail_1/output/Lecture_5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the number a ans b: ";
+    cin>>a>>b;
+    cout<<"a > b = "<<(a>b)<<endl;
+    //AND operation
+
+    cout<<"a && b = "<<(a&&b)<<endl;
+
+    //OR operation
+    cout<<"a||b = "<<(a||b)<<endl;
+    return 0;
+}
+// output
+// Enter the number a ans b: 5 4
+// 1
