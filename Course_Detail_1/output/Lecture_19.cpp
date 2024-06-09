5507cd3c2f0d32f4a71ef6baff8dc129d1dff4fa
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-06-09 06:16:22 +0545

    size of data type

---
+++ Course_Detail_1/output/Lecture_19.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+   cout<<"The size of int is : "<<sizeof(int)<<endl;
+cout<<"The size of char is : "<<sizeof(char)<<endl;
+   cout<<"The size of string is : "<<sizeof(string)<<endl;
+   cout<<"The size of float is : "<<sizeof(float)<<endl;
+   long long int a;
+   int b;
+   cout<<"The size of int is "<<sizeof(a)<<endl<<"The size of long long int is:"<<sizeof(b);
+
+
+
+   return 0; 
+}
