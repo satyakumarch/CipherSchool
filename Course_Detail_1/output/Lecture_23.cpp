e76594b780231b9f1447195cf3567aa9e2d08515
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-06-09 15:13:18 +0545

    pointer 3

---
+++ Course_Detail_1/output/Lecture_23.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int *x;
+    x=&a;
+    cout<<" "<<x<<"The value of x+1 is"<<x+1<<endl;
+    cout<<x+2;
+    return 0;
+    
+}
