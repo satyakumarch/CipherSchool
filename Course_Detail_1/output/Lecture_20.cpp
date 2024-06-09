269aac90ffebbbdbedf617d3fd149c0d2b36227e
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-06-09 06:32:42 +0545

    adderss of hexadeciamal values

---
+++ Course_Detail_1/output/Lecture_20.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 42;
+    cout << "Address of the hexadecimal number " << hex << num << ": " << &num << endl;
+    return 0;
+}
