170db7f8e034321ad0690625bbe39b87a9ca17d5
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-06-30 16:17:19 +0545

    string array

--- Course_Detail_1/output/Lecture_26.cpp
+++ Course_Detail_1/output/Lecture_26.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    char name[5];
+    char name[1000];
     cout<<"Enter your string:";
     for(int i=0;i<5;i++){
         cin>>name[i];
---
+++ Course_Detail_1/output/Lecture_27.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string a;
+    getline(cin,a);
+
+    cout<<"Your full name is:"<<a<<endl;
+    // string a,b;
+    // cout<<"Enter the value of a and b:";
+    // cin>>a>>b;
+    // cout<<"The string which you have given as input are :"<<a<<" "<<b<<endl;;
+    // string c;
+    // c=a+b;
+    // cout<<"the addition of two string a and b is:"<<c;
+}
