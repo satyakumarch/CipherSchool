176f43b8d0db60f98a041e7a94291487251085b1
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-05 16:14:14 +0545

    class and object part 2

---
+++ Course_Detail_1/output/Lecture_38.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    public:
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+};
+class Vehicle
+{
+    public:
+    string car_name;
+    string tyre_name;
+    int number_of_tyre;
+    int number_of_seat;
+};
+
+
+int main() {
+   Student a,b,c;
+   int d,e,f;
+   Vehicle hyundai,suzuki,ferrari;
+}
